1a065737ad35e392297a9bc1b1553c9b04b162f4
y-kawagu <58983717+y-kawagu@users.noreply.github.com>
2020-07-20 01:43:26 +0900

    Translate and erase japanese comments

--- evaluator.py
+++ evaluator.py
@@ -21,16 +21,15 @@ from sklearn import metrics
 ########################################################################
 # constant values
 ########################################################################
-# #eval_data_listから機種名の行を調べるため用
+# Number of columns in lines indicating machine types in the ground truth CSV + 1
 CHK_MACHINE_TYPE_LINE = 2
 
-# リストのカラムナンバー
-# #eval_data_csvのカラムナンバー
+# Column index in the ground truth CSV
 FILENAME_COL = 0
 MACHINE_TYPE_COL = 0
 Y_TRUE_COL = 2
 
-# #anomaly_score_csvのカラムナンバー
+# Column index in anomaly score CSVs
 EXTRACTION_ID_COL = 0
 SCORE_COL = 1
 ########################################################################
@@ -39,17 +38,16 @@ SCORE_COL = 1
 ########################################################################
 # parameters
 ########################################################################
-# #pAUCの計算用
+# FPR threshold for pAUC
 MAX_FPR = 0.1
 
-# 読み込みと出力の設定
-# #nomalかanomalyかの情報が入ったCSVの読取用
+# Path of the ground truth
 EVAL_DATA_LIST_PATH = "./eval_data_list.csv"
 
-# #anomaly score csvを格納しているフォルダ群のルートディレクトリ
+# Directory in which each team's subdirectory containing the anomaly scores
 TEAMS_ROOT_DIR = "./teams/"
 
-# #result_<team_name>.csvの出力ディレクトリ
+# Output directory
 RESULT_DIR = "./teams/"
 ########################################################################
 
@@ -70,53 +68,39 @@ def save_csv(save_file_path,
 ########################################################################
 def output_result(team_dir, machine_types):
 
-    # team_dirのフォルダ名を取得
     dir_name = os.path.basename(team_dir)
 
-    # #AUC,pAUCを出力するファイル名
     result_name = "result_" + dir_name + ".csv"
 
-    # #出力先path
     result_file = "{result_dir}/{result_name}".format(result_dir=RESULT_DIR, result_name=result_name)
 
-    # CSV出力用リストを宣言
     csv_lines = []
 
-    # 機種ごとにAUC、pAUCの算出を行う
     for machine_type in machine_types:
 
-        # 機種名を使ってanomaly_scoreファイル名を取得
         anomaly_score_path_list = sorted(
             glob.glob("{dir}/anomaly_score_{machine_type}_id*".format(dir=team_dir, machine_type=machine_type)))
 
-        # CSV出力用リストへ機種名とタグを追加
         csv_lines.append([machine_type])
         csv_lines.append(["id", "AUC", "pAUC"])
 
-        # AUCとpAUCの平均計算用リストを宣言
         performance = []
         print("=============================================")
         print("MACHINE TYPE IS [{}]".format(machine_type))
         print("---------------------------------------------")
 
-        # idごとのAUC,pAUCの算出
         for anomaly_score_path in anomaly_score_path_list:
 
-            # 各anomaly_scoreファイルの中身をリストへ格納
             with open(anomaly_score_path) as fp:
                 anomaly_score_list = list(csv.reader(fp))
 
-                # ファイル名を昇順でソート
                 anomaly_score_list_sort = sorted(anomaly_score_list, key=itemgetter(0))
 
-            # idの抽出
             machine_id = re.findall('id_[0-9][0-9]', anomaly_score_path)[EXTRACTION_ID_COL]
             print(machine_id)
 
-            # normalかanomalyの情報格納用
             y_true = []
 
-            # 機種idと機種名を使用してeval_data_listからnormalかanomalyの情報を実数に変換して抽出
             for eval_data in eval_data_list:
                 if len(eval_data) < CHK_MACHINE_TYPE_LINE:
                     flag = True if eval_data[MACHINE_TYPE_COL] == machine_type else False
@@ -124,15 +108,12 @@ def output_result(team_dir, machine_types):
                     if flag and machine_id in str(eval_data[FILENAME_COL]):
                         y_true.append(float(eval_data[Y_TRUE_COL]))
 
-            # anomaly_scoreを実数に変換して抽出
             y_pred = [float(anomaly_score[SCORE_COL]) for anomaly_score in anomaly_score_list_sort]
 
-            # ここで数が一致していない場合エラー
             if len(y_true) != len(y_pred):
                 print("Err:anomaly_score may be missing")
                 sys.exit(1)
 
-            # AUCとpAUCの算出を行いid単位で出力用リストへ格納、機種ごとの平均をとるためにリストへ格納
             auc = metrics.roc_auc_score(y_true, y_pred)
             p_auc = metrics.roc_auc_score(y_true, y_pred, max_fpr=MAX_FPR)
             csv_lines.append([machine_id.split("_", 1)[1], auc, p_auc])
@@ -140,7 +121,6 @@ def output_result(team_dir, machine_types):
             print("AUC :", auc)
             print("pAUC :", p_auc)
 
-        # 平均算出し出力用リストへ格納
         averaged_performance = numpy.mean(numpy.array(performance, dtype=float), axis=0)
         print("\nAUC Average :", averaged_performance[0])
         print("pAUC Average :", averaged_performance[1])
@@ -158,10 +138,8 @@ def output_result(team_dir, machine_types):
 ########################################################################
 if __name__ == "__main__":
 
-    # teamsディレクトリ内にある複数のチームに対して処理を行う
     teams_dirs = glob.glob("{root_dir}/*".format(root_dir=TEAMS_ROOT_DIR))
 
-    # eval_data_listの読込
     if os.path.exists(EVAL_DATA_LIST_PATH):
         with open(EVAL_DATA_LIST_PATH) as fp:
             eval_data_list = list(csv.reader(fp))
@@ -169,15 +147,12 @@ if __name__ == "__main__":
         print("Err:eval_data_list.csv not found")
         sys.exit(1)
 
-    # 機種名リストを宣言
     machine_types = []
 
-    # 機種名リストにeval_data_listから機種名を格納
     for idx in eval_data_list:
         if len(idx) < CHK_MACHINE_TYPE_LINE:
             machine_types.append(idx[MACHINE_TYPE_COL])
 
-    # team単位で処理を行う
     for team_dir in teams_dirs:
         if os.path.isdir(team_dir):
             output_result(team_dir, machine_types)
